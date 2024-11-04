23617e9874acdccbc2d0df62a6ecf608c41b1122
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2024-11-04 22:44:16 +0500

    add super user

--- firstapp/settings.py
+++ firstapp/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'firstapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'firstapp_db',          # Replace with your PostgreSQL database name
+        'USER': 'postgres',           # Replace with your PostgreSQL username
+        'PASSWORD': 'postgres',   # Replace with your PostgreSQL password
+        'HOST': 'localhost',              # Set to 'localhost' for local development
+        'PORT': '5432',                   # Default PostgreSQL port
     }
 }
 
